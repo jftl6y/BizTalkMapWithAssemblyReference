827a3ca459c613ceff9bd57f7dcefe9e583da231
John Scott (Azure) <joscot@microsoft.com>
2020-08-31 11:04:10 -0700

    Initial Commit

---
+++ MapWithAssemblyReference/MapperAssemblies/TransformValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperAssemblies
+{
+    public class TransformValues
+    {
+        public string AddTodayToValue(string input)
+        {
+            return $"{input} today is {DateTime.Now.DayOfWeek.ToString()}";
+        }
+        public string ConcatValues(string val1, string val2)
+        {
+            return string.Concat(val1, val2);
+        }
+    }
+}
